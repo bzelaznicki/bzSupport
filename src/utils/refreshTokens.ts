f2c92b6a0383e453c105a66a3a89ca6ad3f6bead
bzelaznicki <bartosz@zelaznicki.com>
2025-07-31 22:46:23 +0200

    BZS-103 added explicit import

--- src/utils/refreshTokens.ts
+++ src/utils/refreshTokens.ts
@@ -1,4 +1,5 @@
 import { Buffer } from "node:buffer";
+import { crypto } from "https://deno.land/std@0.160.0/crypto/mod.ts";
 
 export function generateRefreshToken(): string {
   const bytes = new Uint8Array(64);
